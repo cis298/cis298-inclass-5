98597693eb0a05ae58da1b7f92fcf21faf942d50
David Barnes <dbarnes@kvcc.edu>
2019-02-27 17:20:19 -0500

    Add RecyclerView as dependency. Add RecyclerView to CrimeList Fragment. Add CrimeHolder and CrimeAdapter inner classes of CrimeListFragment to work with the RecyclerView. Update Manifest to make CrimeList the starting Activity.

--- app/src/main/java/edu/kvcc/cis298/criminalintent/CrimeListFragment.java
+++ app/src/main/java/edu/kvcc/cis298/criminalintent/CrimeListFragment.java
@@ -1,7 +1,159 @@
 package edu.kvcc.cis298.criminalintent;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class CrimeListFragment extends Fragment {
-    //Nothing yet.
+    private RecyclerView mCrimeRecyclerView;
+    private CrimeAdapter mAdapter;
+
+    @Nullable
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater,
+            @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState
+    ) {
+        // Inflate the layout file for this fragment
+        View view = inflater.inflate(
+                R.layout.fragment_crime_list,
+                container,
+                false
+        );
+
+        // Get a handle to the recycler view
+        mCrimeRecyclerView = (RecyclerView) view
+            .findViewById(R.id.crime_recycler_view);
+        // Set the LayoutManager. Without this, the
+        // app would just crash.
+        mCrimeRecyclerView.setLayoutManager(
+                new LinearLayoutManager(
+                        getActivity()
+                )
+        );
+
+        updateUI();
+
+        // Return the view now that things are wired up.
+        return view;
+    }
+
+    private void updateUI() {
+        // Get the singleton of crimes
+        CrimeLab crimeLab = CrimeLab.get(getActivity());
+        // Pull out the list of crimes from the singleton
+        List<Crime> crimes = crimeLab.getCrimes();
+        // Create a new adapter sending over the list of crimes
+        mAdapter = new CrimeAdapter(crimes);
+        // Set the adapter for the RecyclerView as the adapter we
+        // just created.
+        mCrimeRecyclerView.setAdapter(mAdapter);
+
+        // If you don't want anyone to be able to read your code
+        // do this! It is the same as the above 4 lines.
+        //mCrimeRecyclerView.setAdapter(new CrimeAdapter(crimeLab.get(getActivity()).getCrimes()));
+    }
+
+    private class CrimeHolder
+            extends RecyclerView.ViewHolder
+            implements View.OnClickListener
+    {
+        private TextView mTitleTextView;
+        private TextView mDateTextView;
+
+        private Crime mCrime;
+
+        public CrimeHolder(
+                LayoutInflater inflater,
+                ViewGroup parent
+        ) {
+            super(
+                    inflater.inflate(R.layout.list_item_crime,
+                            parent,
+                            false)
+            );
+            // itemView is defined in RecyclerView. We did not define this,
+            // but we are using it.
+            // When the super constructor above inflates the layout file
+            // it stores it in itemView
+            //
+            // We are setting the onClickListenter for that layout to this
+            // CrimeHolder class.
+            itemView.setOnClickListener(this);
+
+            mTitleTextView = (TextView) itemView.findViewById(
+                    R.id.crime_title
+            );
+            mDateTextView = (TextView) itemView.findViewById(
+                    R.id.crime_date
+            );
+        }
+
+        public void bind(Crime crime) {
+            mCrime = crime;
+            mTitleTextView.setText(mCrime.getTitle());
+            mDateTextView.setText(mCrime.getDate().toString());
+        }
+
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(
+                    getActivity(),
+                    mCrime.getTitle() + " clicked!",
+                    Toast.LENGTH_SHORT
+            ).show();
+        }
+    }
+
+    private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder>
+    {
+        private List<Crime> mCrimes;
+
+        public CrimeAdapter(List<Crime> crimes) {
+            mCrimes = crimes;
+        }
+
+
+        @NonNull
+        @Override
+        public CrimeHolder onCreateViewHolder(
+                @NonNull ViewGroup parent,
+                int viewType
+        ) {
+            // Get the activities layout inflator so that we
+            // can use it send to the CrimeHolder, who will
+            // then in turn use it to inflate the CrimeHolder's
+            // view.
+            LayoutInflater layoutInflater = LayoutInflater.from(
+                    getActivity()
+            );
+
+            return new CrimeHolder(layoutInflater, parent);
+        }
+
+        @Override
+        public void onBindViewHolder(
+                @NonNull CrimeHolder holder,
+                int position
+        ) {
+            Crime crime = mCrimes.get(position);
+            holder.bind(crime);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mCrimes.size();
+        }
+    }
 }
