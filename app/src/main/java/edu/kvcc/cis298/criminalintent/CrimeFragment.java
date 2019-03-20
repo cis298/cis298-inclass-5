bca6f7cdc952fd53d9980d0d6128d36524b41b9b
David Barnes <dbarnes@kvcc.edu>
2019-03-20 17:16:23 -0400

    Add date picker dialog to be able to update a crime's date.

--- app/src/main/java/edu/kvcc/cis298/criminalintent/CrimeFragment.java
+++ app/src/main/java/edu/kvcc/cis298/criminalintent/CrimeFragment.java
@@ -1,9 +1,13 @@
 package edu.kvcc.cis298.criminalintent;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -14,6 +18,7 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class CrimeFragment extends Fragment {
@@ -21,6 +26,8 @@ public class CrimeFragment extends Fragment {
     // Key no for activity, so not operating at OS level, so no need
     // to include the package name in the string.
     private static final String ARG_CRIME_ID = "crime_id";
+    private static final String DIALOG_DATE = "DialogDate"; // For Dialog
+    private static final int REQUEST_DATE = 0;
 
     private Crime mCrime;
     private EditText mTitleField;
@@ -102,7 +109,25 @@ public class CrimeFragment extends Fragment {
         // functionality for the button will be added much later.
         mDateButton = (Button) v.findViewById(R.id.crime_date);
         mDateButton.setText(mCrime.getDate().toString());
-        mDateButton.setEnabled(false);
+
+        //****************************************************
+        // This is Dialog code here. Not needed for assignment
+        //****************************************************
+        mDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager manager = getFragmentManager();
+                //Ask the DialogFragment for fragment with the newInstance method
+                DatePickerFragment dialog = DatePickerFragment.newIntance(mCrime.getDate());
+                // Set the target fragment to this fragment for when the dialog finishes.
+                // Also send over the REQUEST Code declared at the top.
+                dialog.setTargetFragment(CrimeFragment.this, REQUEST_DATE);
+                // Tell the fragment to show. Send over the string DIALOG_DATE as a key that
+                // the fragment manager will use for keeping track of the fragment.
+                dialog.show(manager, DIALOG_DATE);
+            }
+        });
+        //***************************************************
 
         mSolvedCheckbox = (CheckBox)v.findViewById(R.id.crime_solved);
         mSolvedCheckbox.setChecked(mCrime.isSolved());
@@ -117,5 +142,29 @@ public class CrimeFragment extends Fragment {
         });
 
         return v;
+    }
+
+    //****************************************************
+    // This is Dialog code here. Not needed for assignment
+    //****************************************************
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // If the result code is not okay, then there is nothing to do. Just return
+        if (resultCode != Activity.RESULT_OK) {
+            return;
+        }
+
+        // If we are returning from the date picker fragment
+        if (requestCode == REQUEST_DATE) {
+            // Fetch the date from the data
+            Date date = (Date) data.getSerializableExtra(
+                    DatePickerFragment.EXTRA_DATE
+            );
+            // Set the date on the Crime and update the text.
+            mCrime.setDate(date);
+            mDateButton.setText(mCrime.getDate().toString());
+        }
+
     }
 }
